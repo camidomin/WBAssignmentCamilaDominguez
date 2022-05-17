c7a785b326e89cca5df681ad679ca1dd90b0ed68
Cami Dominguez <camiladominguez@MacBook-Air-de-Cami.local>
2022-05-17 15:13:19 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+	"use strict";
+	
+    // CAROUSEL BANNER
+    $(".carousel-sliders").owlCarousel({
+		animateOut: 'fadeOut',
+    	animateIn: 'flipInX',
+	    loop: true,
+	    autoplay: true,
+        autoplayTimeout: 5000,
+	    nav: true,
+	    dots: false,
+	    navContainer: '.banner .custom-nav',
+	    items: 1,
+	});
+
+	// CAROUSEL TESTIMONIALS
+	$(".carousel-testimonials").owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		items: 1
+	});
+});
+
+window.onscroll = function() {toggleMenu()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop + navbar.offsetHeight;
+
+function toggleMenu() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("fixed")
+  } else {
+    navbar.classList.remove("fixed");
+  }
+}
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+  });
